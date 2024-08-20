ed7f1222e077d6ddede4cace05d8bd1388bea25b
sasa2754 <sasamor5@gmail.com>
2024-08-20 17:04:05 -0300

    colorido

--- script.js
+++ script.js
@@ -9,8 +9,16 @@ document.body.addEventListener('mousemove', (event) => {
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
 
-    // Define a cor e desenha o retângulo no canvas
-    ctx.fillStyle = 'black'; // Define a cor do retângulo
+    // Gera valores de cor RGB baseados na posição do mouse
+    // Você pode ajustar os valores 255 e 360 para obter diferentes resultados
+    // const r = Math.floor((x / canvas.width) * 255);
+    // const g = Math.floor((y / canvas.height) * 255);
+    // const b = Math.floor(((x + y) / (canvas.width + canvas.height)) * 255);
+    const r = 30;
+    const g = 126;
+    const b = 12;
+    // Define a cor do retângulo usando a cor RGB calculada
+    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
     ctx.fillRect(x - 10, y - 10, 20, 20);
 
     // Atualiza o texto do header com as coordenadas do mouse
