95716f2742d80f1c4dbc11f1f4b9c43f0d468ffe
Sabrina Mortean <160642163+sasa2754@users.noreply.github.com>
2024-08-23 15:31:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const canvas = document.getElementById('canvas');
 const escrito = document.getElementById('escrito');
-const aumenta = document.getElementById('aumenta');
-const diminui = document.getElementById('diminui');
+const aumentaX = document.getElementById('aumentaX');
+const diminuiX = document.getElementById('diminuiX');
+const aumentaY = document.getElementById('aumentaY');
+const diminuiY = document.getElementById('diminuiY');
+const barrinha = document.getElementById('tamanho');
 
 const ctx = canvas.getContext('2d');
 
@@ -12,11 +15,27 @@ let r = Math.floor(Math.random() * (max - min) + min);
 let g = Math.floor(Math.random() * (max - min) + min);
 let b = Math.floor(Math.random() * (max - min) + min);
 
+console.log(`r: ${r} g: ${g} b: ${b}`);
+
 let nsei = 0;
 let nsei2 = 0;
 let nsei3 = 0;
 
 let tamanho = 10;
+
+let desenho = false;
+
+function ativar() {
+    desenho = true;
+}
+
+function desativar() {
+    desenho = false;
+}
+
+
+
+
 
 // document.body.addEventListener('keydown')
 // Adiciona o evento de movimento do mouse ao body
@@ -25,75 +44,100 @@ document.body.addEventListener('mousemove', (event) => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-
-    if(nsei == 0){
-        r += 5;
+    
+    console.log(barrinha.value);
+    if(desenho == true){
+    
+        if(nsei == min){
+            r += 5;
+        }
+        else{
+            r -= 12;
+        }
+        if(nsei2 == min){
+            g += 10;
+        }
+        else{
+            g -= 9;
+        }
+        if(nsei3 == min){
+            b += 15;
+        }
+        else{
+            b -= 8;
+        }
+    
+        if (r >= max) {
+            nsei = 1;
+            r = max;
+        }
+        if (g >= max) {
+            nsei2 = 1;
+            g = max;
+        }
+        if (b >= max) {
+            nsei3 = 1;
+            b = max;
+        }
+        if(r <= min){
+            nsei = 0;
+            r = min;
+        }
+        if (g <= min) {
+            nsei2 = 0;
+            g = min;
+        }
+        if (b <= min) {
+            nsei3 = 0;
+            b = min;
+        }
+    
+        // Gera valores de cor RGB baseados na posição do mouse
+        // Você pode ajustar os valores 255 e 360 para obter diferentes resultados
+        // const r = Math.floor((x / canvas.width) * 255);
+        // const g = Math.floor((y / canvas.height) * 255);
+        // const b = Math.floor(((x + y) / (canvas.width + canvas.height)) * 255);
+        // Define a cor do retângulo usando a cor RGB calculada
+        ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+        ctx.fillRect(x - (tamanho/2), y - (tamanho/2), tamanho, tamanho);
+        // ctx.fillRect(x - 10, y - 10, 10, 10000);
+    
     }
-    else{
-        r -= 12;
-    }
-    if(nsei2 == 0){
-        g += 10;
-    }
-    else{
-        g -= 9;
-    }
-    if(nsei3 == 0){
-        b += 15;
-    }
-    else{
-        b -= 8;
-    }
-
-    if (r >= 255) {
-        nsei = 1;
-        r = 255;
-    }
-    if (g >= 255) {
-        nsei2 = 1;
-        g = 255;
-    }
-    if (b >= 255) {
-        nsei3 = 1;
-        b = 255;
-    }
-    if(r <= 0){
-        nsei = 0;
-        r = 0;
-    }
-    if (g <= 0) {
-        nsei2 = 0;
-        g = 0;
-    }
-    if (b <= 0) {
-        nsei3 = 0;
-        b = 0;
-    }
-
-    // Gera valores de cor RGB baseados na posição do mouse
-    // Você pode ajustar os valores 255 e 360 para obter diferentes resultados
-    // const r = Math.floor((x / canvas.width) * 255);
-    // const g = Math.floor((y / canvas.height) * 255);
-    // const b = Math.floor(((x + y) / (canvas.width + canvas.height)) * 255);
-    // Define a cor do retângulo usando a cor RGB calculada
-    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-    ctx.fillRect(x - (tamanho/2), y - (tamanho/2), tamanho, tamanho);
-
+    
     // Atualiza o texto do header com as coordenadas do mouse
     escrito.textContent = `X: ${Math.round(x)}, Y: ${Math.round(y)}`;
 });
 
-function aumentar() {
-    tamanho += 10;
-
-    if (tamanho >= 650) {
-        tamanho = 650;
-    }
+function mudarTamanho(){
+    tamanho = barrinha.value * 6;
 }
 
-function diminuir() {
-    tamanho -= 10;
-    if (tamanho <= 0) {
-        tamanho = 2;
-    }
-}
+// function aumentarX() {
+//     tamanhoX += 10;
+
+//     if (tamanhoX >= 650) {
+//         tamanhoX = 650;
+//     }
+// }
+
+// function diminuirX() {
+//     tamanhoX -= 10;
+//     if (tamanhoX <= 0) {
+//         tamanhoX = 2;
+//     }
+// }
+
+// function aumentarY() {
+//     tamanhoY += 10;
+
+//     if (tamanhoY >= 650) {
+//         tamanhoY = 650;
+//     }
+// }
+
+// function diminuirY() {
+//     tamanhoY -= 10;
+//     if (tamanhoY <= 0) {
+//         tamanhoY = 2;
+//     }
+// }
