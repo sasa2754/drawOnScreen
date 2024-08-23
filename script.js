92bfc931f61454590b2e11f0c780f264f404576e
Sabrina Mortean <160642163+sasa2754@users.noreply.github.com>
2024-08-23 17:10:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const canvas = document.getElementById('canvas');
 const escrito = document.getElementById('escrito');
-const aumentaX = document.getElementById('aumentaX');
-const diminuiX = document.getElementById('diminuiX');
-const aumentaY = document.getElementById('aumentaY');
-const diminuiY = document.getElementById('diminuiY');
 const barrinha = document.getElementById('tamanho');
+const color = document.getElementById('color');
+
 
 const caneta = canvas.getContext('2d');
 
@@ -23,104 +21,112 @@ let nsei3 = 0;
 let tamanho = 10;
 
 let desenho = false;
+
 let apagar = false;
+
+let isColorido = true;
 
 let cor;
 
-function ativar() {
-    desenho = true;
-}
-
-function desativar() {
-    desenho = false;
-}
-
-function borracha() {
-    apagar = true;
-}
-
-function pincel() {
-    apagar = false;
-}
 
 
-
-
-
-// document.body.addEventListener('keydown')
 // Adiciona o evento de movimento do mouse ao body
 document.body.addEventListener('mousemove', (event) => {
     // Obtém a posição do mouse relativa ao canvas
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    cor = `rgb(${r}, ${g}, ${b})`;
     
-    console.log(barrinha.value);
+    if(isColorido == true){
+        cor = `rgb(${r}, ${g}, ${b})`;
+        colorido();
+    }
+    else {
+        cor = color.value;
+    }
+    
     if(desenho == true){
-    
-        if(nsei == min){
-            r += 5;
-        }
-        else{
-            r -= 12;
-        }
-        if(nsei2 == min){
-            g += 10;
-        }
-        else{
-            g -= 9;
-        }
-        if(nsei3 == min){
-            b += 15;
-        }
-        else{
-            b -= 8;
-        }
-    
-        if (r >= max) {
-            nsei = 1;
-            r = max;
-        }
-        if (g >= max) {
-            nsei2 = 1;
-            g = max;
-        }
-        if (b >= max) {
-            nsei3 = 1;
-            b = max;
-        }
-        if(r <= min){
-            nsei = 0;
-            r = min;
-        }
-        if (g <= min) {
-            nsei2 = 0;
-            g = min;
-        }
-        if (b <= min) {
-            nsei3 = 0;
-            b = min;
-        }
-    
-        // Define a cor do retângulo usando a cor RGB calculada
+        // Define a cor do retângulo
         if (apagar == false) {
             caneta.fillStyle = cor;
-            // caneta.strokeStyle = cor;
         }
         else {
             caneta.fillStyle = `black`;
-            // caneta.strokeStyle = `white`;
         }
 
+        // Desenha o quadrado
         caneta.fillRect(x - (tamanho/2), y - (tamanho/2), tamanho, tamanho);
-        // caneta.strokeRect(x - (tamanho/2), y - (tamanho/2), tamanho, tamanho);
     }
     
     // Atualiza o texto do header com as coordenadas do mouse
     escrito.textContent = `X: ${Math.round(x)}, Y: ${Math.round(y)}`;
 });
 
+// Muda o tamanho do pincel
 function mudarTamanho(){
     tamanho = barrinha.value * 6;
+}
+
+// Faz o pincel só pintar quando clica
+function ativar() {
+    desenho = true;
+}
+function desativar() {
+    desenho = false;
+}
+
+// Troca do colorido pra cor fixa e vice versa
+function trocarCor(){
+    isColorido = !isColorido;
+}
+
+// Liga a borracha
+function borracha() {
+    apagar = !apagar;
+}
+
+function colorido() {
+    if(nsei == min){
+        r += 5;
+    }
+    else{
+        r -= 12;
+    }
+    if(nsei2 == min){
+        g += 10;
+    }
+    else{
+        g -= 9;
+    }
+    if(nsei3 == min){
+        b += 15;
+    }
+    else{
+        b -= 8;
+    }
+    
+    if (r >= max) {
+        nsei = 1;
+        r = max;
+    }
+    if (g >= max) {
+        nsei2 = 1;
+        g = max;
+    }
+    if (b >= max) {
+        nsei3 = 1;
+        b = max;
+    }
+    if(r <= min){
+        nsei = 0;
+        r = min;
+    }
+    if (g <= min) {
+        nsei2 = 0;
+        g = min;
+    }
+    if (b <= min) {
+        nsei3 = 0;
+        b = min;
+    }
 }
